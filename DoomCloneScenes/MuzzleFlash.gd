b8f9d3f2836998075a39e4f0a09bc6553adf31ea
FirePrehistoric2 <zandarbeker@gmail.com>
2022-06-10 12:49:21 +1200

    Finished Cycle 1
    Yay :D
    
    Added:
    Shotgun + Pistol
    Basic Enemy (AI Included)
    Test Level
    Movement
    Basic Animations

---
+++ DoomCloneScenes/MuzzleFlash.gd
@@ -0,0 +1,8 @@
+extends OmniLight
+
+func _ready():
+	pass
+
+
+func _on_Timer_timeout():
+	queue_free()
--- DoomCloneScenes/Player.gd
+++ DoomCloneScenes/Player.gd
@@ -1,23 +1,59 @@
 extends KinematicBody
 
 var velocity = Vector3()
+var gravity = -30
+var max_speed = 8
+var mouse_sensitivity = 0.002
+
+
+onready var pistol = preload("res://DoomCloneScenes/Pistol.tscn")
+onready var shotgun = preload("res://DoomCloneScenes/Shotgun.tscn")
+var current_gun = 0
+onready var carried_guns = [pistol,shotgun]
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func get_input():
-	var input_dlr = Vector3()
-	if Input.is_action_press("move forward"):
-		input_dlr += -global_transform.basis.z
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forward"):
+		input_dir += -global_transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		input_dir += global_transform.basis.z
+	if Input.is_action_pressed("strafe_left"):
+		input_dir += -global_transform.basis.x
+	if Input.is_action_pressed("strafe_right"):
+		input_dir += global_transform.basis.x
+	return input_dir
 
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2,1.2)
 	
 func _physics_process(delta):
-	pass
-	
+	velocity.y += gravity * delta 
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+
 func change_gun(gun):
-	pass
-	
+	$Pivot/Gun.get_child(0).queue_free()
+	var new_gun = carried_guns[gun].instance()
+	$Pivot/Gun.add_child(new_gun)
+
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("next_gun"):
+		current_gun+= 1 
+		if current_gun > len(carried_guns)-1:
+			current_gun = 0
+		change_gun(current_gun)
+	elif Input.is_action_just_pressed("prev_gun"):
+		current_gun -= 1
+		if current_gun < 0:
+			current_gun = len(carried_guns) -1
+		change_gun(current_gun)
+		
